d7a4d55502f65510987443268f4c519c12e130eb
Suraj <surajkumar15081947@gmail.com>
2023-07-06 11:42:01 +0530

    chk

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
 
+let density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+
 let img;
 
-function preload(){
-    img = loadImage("lord shiva.webp")
+function preload() {
+  img = loadImage("IMG20220809154015.jpg");
+}
+
+function setup() {
+  noCanvas();
+  gloria.loadPixels();
+  for (let j = 0; j < gloria.height; j++) {
+    let row = "";
+    for (let i = 0; i < gloria.width; i++) {
+      const pixelIndex = (i + j * gloria.width) * 4;
+      const r = gloria.pixels[pixelIndex + 0];
+      const g = gloria.pixels[pixelIndex + 1];
+      const b = gloria.pixels[pixelIndex + 2];
+
+      const avg = (r + g + b) / 3;
+
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, len, 0));
+
+      const c = density.charAt(charIndex);
+      if (c == " ") row += "&nbsp;";
+      else row += c;
+    }
+    createDiv(row);
+  }
 }
