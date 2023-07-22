702c2376362580680f8f57f667b6aefd13a53e2c
Suraj <surajkumar15081947@gmail.com>
2023-07-22 11:29:44 +0530

    video

--- app.js
+++ app.js
@@ -1,87 +1,94 @@
-const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+(function () {
+    const video = document.getElementById('video')
+    const textVideo = document.getElementById('text-video')
+    const canvas = document.getElementById('canvas-video')
+    const ctx = canvas.getContext('2d');
+    const width = 320 / 2, height = 240 / 2;
 
-let gloria;
-
-function preload() {
-  gloria = loadImage("Rfa.jpg");
-}
-
-function setup() {
-  noCanvas();
-  gloria.loadPixels();
-  for (let j = 0; j < gloria.height; j++) {
-    let row = "";
-    for (let i = 0; i < gloria.width; i++) {
-      const pixelIndex = (i + j * gloria.width) * 4;
-      const r = gloria.pixels[pixelIndex + 0];
-      const g = gloria.pixels[pixelIndex + 1];
-      const b = gloria.pixels[pixelIndex + 2];
-
-      const avg = (r + g + b) / 3;
-
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
-
-      const c = density.charAt(charIndex);
-      if (c == " ") row += "&nbsp;";
-      else row += c;
-    }
-    createDiv(row);
-  }
-}
-
-
-function draw(){
-  image = loadImage(resizedImage)
-  console.log(image)
-  image.loadPixels();
-  for (let j = 0; j < image.height; j++) {
-    let row = "";
-    for (let i = 0; i < image.width; i++) {
-      const pixelIndex = (i + j * image.width) * 4;
-      const r = image.pixels[pixelIndex + 0];
-      const g = image.pixels[pixelIndex + 1];
-      const b = image.pixels[pixelIndex + 2];
-
-      const avg = (r + g + b) / 3;
-
-      const len = density.length;
-      const charIndex = floor(map(avg, 0, 255, len, 0));
-
-      const c = density.charAt(charIndex);
-      if (c == " ") row += "&nbsp;";
-      else row += c;
-    }
-    createDiv(row);
-  }
-}
-
-function resizeImage() {
-  var input = document.getElementById('imageInput');
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  
-  var file = input.files[0];
-  var reader = new FileReader();
-
-  reader.onload = function(e) {
-    var img = new Image();
-
-    img.onload = function() {
-      var width = 50;
-      var height = 50;
-      canvas.width = width;
-      canvas.height = height;
-
-    //  ctx.drawImage(img, 0, 0, width, height);
-
-      resizedImage = canvas.toDataURL('image/jpeg');
-      
-    };
-
-    img.src = e.target.result;
+    const gradient = "_______.:!/r(l1Z4H9W8$@";
+    const preparedGradient = gradient.replaceAll('_', '\u00A0')
     
-    draw();
-  };
-  reader.readAsDataURL(file);
-}
+
+    const init = () => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+            .then(function (stream) {
+                video.srcObject = stream;
+                video.play();
+            })
+            .catch(function (err) {
+                console.log("An error occurred: " + err);
+            });
+    }
+
+    const clearphoto = () => {
+        ctx.fillStyle = "#fff";
+        ctx.fillRect(0, 0, width, height);
+    }
+
+    const render = (ctx) => {
+        if (width && height) {
+            canvas.width = width;
+            canvas.height = height;
+            ctx.drawImage(video, 0, 0, width, height);
+        } else {
+            clearphoto();
+        }
+    }
+
+    const getPixelsGreyScale = (ctx) => {
+        const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        const data = imageData.data;
+        let row = 0
+        const res = new Array(height).fill(0).map(() => []);
+        for (let i = 0, c = 0; i < data.length; i += 4) {
+            const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+            let curr = res[row]
+            curr.push(avg)
+            if (c < width) {
+                c++
+            }
+            if (c === width) {
+                c = 0
+                row += 1
+            }
+
+        }
+        return res
+    }
+
+    const getCharByScale = (scale) => {
+        const val = Math.floor(scale / 255 * (gradient.length - 1))
+        return preparedGradient[val]
+    }
+
+    const renderText = (node, textDarkScale) => {
+        let txt = `<div>`
+        for (let i = 0; i < textDarkScale.length; i++) {
+            txt += `<span style="color: #fff">`
+            for (let k = 0; k < textDarkScale[i].length; k++) {
+                txt = `${txt}${getCharByScale(textDarkScale[i][k])}`
+            }
+            txt += `</span><br>`
+        }
+        txt += `</div>`
+        node.innerHTML = txt
+    }
+
+    let running = true;
+
+    init()
+    const frame = () => requestAnimationFrame(() => {
+        render(ctx)
+        const chars = getPixelsGreyScale(ctx)
+        renderText(textVideo, chars)
+        if (running)
+            frame()
+    })
+    frame()
+
+
+    document.getElementById('stop').addEventListener('click', (e) => {
+        running = false
+    })
+
+})()
