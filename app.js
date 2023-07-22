2f140424f7776f5cabcadc25ed66c61d07bd3de8
Suraj <surajkumar15081947@gmail.com>
2023-07-22 10:09:32 +0530

    Creating the live video streamer

--- app.js
+++ app.js
@@ -1,3 +1,61 @@
+const density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+
+let gloria;
+
+function preload() {
+  gloria = loadImage("Rfa.jpg");
+}
+
+function setup() {
+  noCanvas();
+  gloria.loadPixels();
+  for (let j = 0; j < gloria.height; j++) {
+    let row = "";
+    for (let i = 0; i < gloria.width; i++) {
+      const pixelIndex = (i + j * gloria.width) * 4;
+      const r = gloria.pixels[pixelIndex + 0];
+      const g = gloria.pixels[pixelIndex + 1];
+      const b = gloria.pixels[pixelIndex + 2];
+
+      const avg = (r + g + b) / 3;
+
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, len, 0));
+
+      const c = density.charAt(charIndex);
+      if (c == " ") row += "&nbsp;";
+      else row += c;
+    }
+    createDiv(row);
+  }
+}
+
+
+function draw(){
+  image = loadImage(resizedImage)
+  console.log(image)
+  image.loadPixels();
+  for (let j = 0; j < image.height; j++) {
+    let row = "";
+    for (let i = 0; i < image.width; i++) {
+      const pixelIndex = (i + j * image.width) * 4;
+      const r = image.pixels[pixelIndex + 0];
+      const g = image.pixels[pixelIndex + 1];
+      const b = image.pixels[pixelIndex + 2];
+
+      const avg = (r + g + b) / 3;
+
+      const len = density.length;
+      const charIndex = floor(map(avg, 0, 255, len, 0));
+
+      const c = density.charAt(charIndex);
+      if (c == " ") row += "&nbsp;";
+      else row += c;
+    }
+    createDiv(row);
+  }
+}
+
 function resizeImage() {
   var input = document.getElementById('imageInput');
   var canvas = document.getElementById('canvas');
@@ -15,11 +73,15 @@ function resizeImage() {
       canvas.width = width;
       canvas.height = height;
 
-      ctx.drawImage(img, 0, 0, width, height);
-      var resizedImage = canvas.toDataURL('image/jpeg');
-      console.log(resizedImage)
+    //  ctx.drawImage(img, 0, 0, width, height);
+
+      resizedImage = canvas.toDataURL('image/jpeg');
+      
     };
+
     img.src = e.target.result;
+    
+    draw();
   };
   reader.readAsDataURL(file);
 }
